62d64ade270d773a46e0a69e7b58536a5d37e692
Gabrielle Fioranelli <49372916+gabifioranelli@users.noreply.github.com>
2020-06-17 15:22:18 -0300

    Add files via upload

---
+++ company.js
@@ -0,0 +1,10 @@
+const company = {
+    name: 'Rocketseat',
+    color: 'purple',
+    focus: 'Programing',
+    adress: {
+        street: 'Rua Guilherme Gembala,',
+        number: 260
+    }
+}
+console.log(`A empresa ${company.name} está localizada na ${company.adress.street} número ${company.adress.number}`)
